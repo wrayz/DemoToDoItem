043801f212354d7cf95cc683d8b462e5112bb9a1
wrayz <ray.wu@tm-robot.com>
2020-03-24 21:03:57 +0800

    init

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<TodoItemDTO> Get()
+        {
+            return new TodoItemDTO 
+            {
+                Id = 1,
+                Name = "Test",
+                IsComplete = true
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<TodoItemDTO> Get(int id)
+        {
+            return new TodoItemDTO
+            {
+                Id = 99,
+                Name = "Test Parameter",
+                IsComplete = true
+            };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<TodoItemDTO> Post(TodoItemDTO item)
+        {
+            return new TodoItemDTO 
+            {
+                Id = 99,
+                Name = item.Name,
+                IsComplete = true
+            };
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult<TodoItemDTO> Put(int id, TodoItemDTO item)
+        {
+            return item;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult<TodoItemDTO> Delete(int id)
+        {
+            return new TodoItemDTO 
+            {
+                Id = id,
+                IsComplete = true
+            };
+        }
+    }
+}
